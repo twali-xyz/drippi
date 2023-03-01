8d8bf9af0d0685239f4a83ce8b0d4ab54a514608
ivyastrix <ivy@ivyastrix.com>
2023-03-01 09:43:46 -0800

    added vercel analytics package to enable audience tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import { theme } from "@/styles/extendedTheme";
+import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
+          <Analytics />
       </ChakraProvider>
     </SessionProvider>
   );
